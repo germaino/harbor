106c2271f5352138a34ad1c7551583d7f2e403d8
Olivier GERMAIN <olivier.germain@schneider-electric.com>
2020-03-29 17:11:57 +0200

    use parameters

--- test_tls.sh
+++ test_tls.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-export HOST="localhost"
+HOST="${1:-localhost}"
+IMAGE="${2:-test/hello-world}"
 
-
-echo "Get token"
+echo "Get token on $HOST"
 export TOKEN=\
 "$(curl \
 --silent \
 --key ./certs/users/germaino.key \
 --cert ./certs/users/germaino.crt \
 -H "Content-Type: application/json" \
-"https://$HOST/service/token?service=harbor-registry&scope=repository:test/hello-world:pull,push" \
+"https://$HOST/service/token?service=harbor-registry&scope=repository:$IMAGE:pull,push" \
 | jq -r '.token' \
 )"
 
-echo "Push image"
+echo "Push image to $HOST"
 
 generate_jSON_STR()
 {
@@ -28,8 +28,8 @@ REGISTRY_AUTH=$(echo $(generate_jSON_STR) | base64)
 REGISTRY_AUTH_NO_SPACE=$(echo ${REGISTRY_AUTH} | tr -d ' ')
 
 
-curl -v -X POST -H X-Registry-Auth:${REGISTRY_AUTH_NO_SPACE} --unix-socket /var/run/docker.sock http://localhost/images/$HOST/test/hello-world/push
+curl -v -X POST -H X-Registry-Auth:${REGISTRY_AUTH_NO_SPACE} --unix-socket /var/run/docker.sock http://localhost/images/$HOST/$IMAGE/push
 #curl -v -X POST -H X-Registry-Auth:${REGISTRY_AUTH_NO_SPACE} --unix-socket /var/run/docker.sock http://localhost/images/create?fromImage=$HOST/test/hello-world
 #curl --silent -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" --key ./certs/users/germaino.key --cert ./certs/users/germaino.crt https://$HOST/v2/test/images/json
-curl --silent -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" --key ./certs/users/germaino.key --cert ./certs/users/germaino.crt https://$HOST/v2/test/hello-world/manifests/latest | jq -r '.fsLayers[].blobSum'
+curl --silent -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" --key ./certs/users/germaino.key --cert ./certs/users/germaino.crt https://$HOST/v2/$IMAGE/manifests/latest | jq -r '.fsLayers[].blobSum'
 
